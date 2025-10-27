b333f262e206c4616938143f636cd734998e2103
skytomo <skytomo221@gmail.com>
2025-10-27 22:19:41 +0900

    style: use spaces

--- directions.py
+++ directions.py
@@ -1,22 +1,22 @@
 def get_opposite_direction(direction):
-	if direction == North:
-		return South
-	elif direction == South:
-		return North
-	elif direction == East:
-		return West
-	elif direction == West:
-		return East
+    if direction == North:
+        return South
+    elif direction == South:
+        return North
+    elif direction == East:
+        return West
+    elif direction == West:
+        return East
 
 def get_all_directions():
-  return [North, East, South, West]
+    return [North, East, South, West]
 
 def direction_to_vector(direction):
-  if direction == North:
-    return (0, 1)
-  elif direction == East:
-    return (1, 0)
-  elif direction == South:
-    return (0, -1)
-  elif direction == West:
-    return (-1, 0)
+    if direction == North:
+        return (0, 1)
+    elif direction == East:
+        return (1, 0)
+    elif direction == South:
+        return (0, -1)
+    elif direction == West:
+        return (-1, 0)
--- main.py
+++ main.py
@@ -3,29 +3,32 @@ import all_pumpkins
 import maze
 
 def main():
-	while True: 
-		for x in range(get_world_size()):
-			for y in range(get_world_size()):
-				# if x % 3 == 1 and y % 3 == 1:
-				#	use_item(Items.Fertilizer)
-				if can_harvest():
-					harvest()
-				if get_water() < 0.5:
-					use_item(Items.Water)
-				if get_pos_x() <= get_world_size() / 8:
-					if get_ground_type() == Grounds.Grassland:
-						till()
-					plant(Entities.Cactus)
-				elif get_pos_x() <= get_world_size() / 4:
-					pass
-				elif get_pos_x() <= get_world_size() / 2:
-					plant(Entities.Tree)
-				else:
-					if get_ground_type() == Grounds.Grassland:
-						till()
-					plant(Entities.Carrot)
-				move(North)
-			move(East)
+    reset_pos()
+    while True:
+        for x in range(get_world_size()):
+            for y in range(get_world_size()):
+                # if x % 3 == 1 and y % 3 == 1:
+                #    use_item(Items.Fertilizer)
+                if can_harvest():
+                    harvest()
+                if get_water() < 0.5:
+                    use_item(Items.Water)
+                if get_pos_x() <= get_world_size() / 8:
+                    if get_ground_type() == Grounds.Grassland:
+                        till()
+                    plant(Entities.Cactus)
+                elif get_pos_x() <= get_world_size() / 4:
+                    pass
+                elif get_pos_x() <= get_world_size() / 2:
+                    plant(Entities.Tree)
+                else:
+                    if get_ground_type() == Grounds.Grassland:
+                        till()
+                    plant(Entities.Carrot)
+                move(North)
+            move(East)
+            if num_drones() < max_drones() and get_world_size() // max_drones() == get_pos_x():
+                spawn_drone(main)
 
 # clear()
 #all_pumpkins.do()
