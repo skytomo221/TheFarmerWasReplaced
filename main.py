83596b3ab94970a840de1506c3764c85b73d76d4
skytomo <skytomo221@gmail.com>
2025-10-27 22:41:48 +0900

    feat: refactor main function to support multiple drones and improve position handling

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from moves import reset_pos
+from moves import move_pos
 import all_pumpkins
 import maze
 
-def main():
-    reset_pos()
+def do(i = 0):
+    x_min = get_world_size() / max_drones() * i
+    x_max = get_world_size() / max_drones() * (i + 1)
+    move_pos((x_min, 0))
     while True:
-        for x in range(get_world_size()):
+        for x in range(x_min, x_max):
             for y in range(get_world_size()):
                 # if x % 3 == 1 and y % 3 == 1:
                 #    use_item(Items.Fertilizer)
@@ -27,10 +29,20 @@ def main():
                     plant(Entities.Carrot)
                 move(North)
             move(East)
-            if num_drones() < max_drones() and get_world_size() // max_drones() == get_pos_x():
-                spawn_drone(main)
+        move_pos((x_min, 0))
 
-# clear()
+def create_worker_task(i = 0):
+    def worker_task():
+        do(i)
+    return worker_task
+
+def main():
+    i = 1
+    while num_drones() < max_drones():
+        spawn_drone(create_worker_task(i))
+        i += 1
+    do()
+
 #all_pumpkins.do()
-# main()
-maze.do()
+main()
+# maze.do()
