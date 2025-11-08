9cfedf931c58bedb3e5d625e816b94ddfddd3c64
skytomo <skytomo221@gmail.com>
2025-11-08 16:18:42 +0900

    feat(calc_pos): add multiplication and division functions for position calculations

--- calc_pos.py
+++ calc_pos.py
@@ -7,5 +7,11 @@ def add_pos(pos1, pos2):
 def sub_pos(pos1, pos2):
     return (pos1[0] - pos2[0], pos1[1] - pos2[1])
 
+def mul_pos(pos, scalar):
+    return (pos[0] * scalar, pos[1] * scalar)
+
+def div_pos(pos, scalar):
+    return (pos[0] / scalar, pos[1] / scalar)
+
 def equal_pos(pos1, pos2):
     return pos1[0] == pos2[0] and pos1[1] == pos2[1]
